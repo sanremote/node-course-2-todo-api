6470fee15b4ba4587371c86445551846ee1fc811
Bruce Polsemen <bpolsemen@gmail.com>
2017-10-29 16:27:50 -0700

    Setup app for hreoku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var options = {useMongoClient: true};
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', options);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', options);
 
 module.exports = {mongoose};
